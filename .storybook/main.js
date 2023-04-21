8d5d58e1a0bb8611aca0d5049dfd4a30045d1a9b
Jordi Burgos <jordiburgos@gmail.com>
2023-04-22 00:07:58 +0200

    Update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/addon-mdx-gfm"],
   "framework": {
     name: "@storybook/react-vite",
     options: {}
